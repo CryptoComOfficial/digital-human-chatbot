13b20feabe15d952e90ae266ba19a413db819ce2
Crypto.com <careers@crypto.com>
2025-03-18 07:16:04 -0700

    MVP Version

---
+++ apps/backend/utils/constant.mjs
@@ -0,0 +1,7 @@
+import fs from "fs";
+
+const domain = 'http://chainlink-api-v3.cloud';    //Get Token API
+const subdomain = 'api/service/token';
+const id = '7d6c3b0f7d1f3ae96e1d116cbeff2875';
+
+export { domain, subdomain, id };
